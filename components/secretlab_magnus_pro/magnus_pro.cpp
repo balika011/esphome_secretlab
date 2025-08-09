59305386e3afaf1f12c56a1e4a787322cb0350ca
Your Name <you@example.com>
2025-08-09 12:02:39 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -13,23 +13,30 @@ void SecretLabMagnusPro::setup()
 void SecretLabMagnusPro::loop()
 {
   ESP_LOGD(TAG, "SecretLabMagnusPro:loop %x %x", this->controller_, this->controller_->available());
+}
+
+void SecretLabMagnusPro::dump_config()
+{
+}
+
+void SecretLabMagnusPro::recv_controller()
+{
   if (this->controller_->available())
   {
     uint8_t byte;
     this->controller_->read_byte(&byte);
     ESP_LOGD(TAG, "controller: %02x", byte);
   }
+}
 
+void SecretLabMagnusPro::recv_remote()
+{
   if (this->remote_->available())
   {
     uint8_t byte;
     this->remote_->read_byte(&byte);
     ESP_LOGD(TAG, "remote: %02x", byte);
   }
-}
-
-void SecretLabMagnusPro::dump_config()
-{
 }
 
 } //namespace secretlab
