a5aba16c3aed2ba371374262a1ecdadc204a3a27
Your Name <you@example.com>
2025-08-09 12:31:19 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -83,6 +83,22 @@ void SecretLabMagnusPro::recv_remote()
   process_remote(msg[0], msg[1]);
 }
 
+uint8_t alpha_7seg[] = { 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x3d, 0x74, 0x30, 0x1e, 0x75, 0x38, 0x15, 0x37, 0x3f, 0x73, 0x67, 0x33, 0x6d, 0x78, 0x3e, 0x2e, 0x2a, 0x76, 0x6e, 0x4b };
+uint8_t num_7seg[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f };
+
+char _7seg_to_char(uint8_t seg)
+{
+  for (int i = 0; i < sizeof(alpha_7seg); i++)
+    if (alpha_7seg[i] == (seg & 0x7f))
+      return 'A' + i;
+
+  for (int i = 0; i < sizeof(num_7seg); i++)
+    if (num_7seg[i] == (seg & 0x7f))
+      return '0' + i;
+
+  return '?';
+}
+
 void SecretLabMagnusPro::process_controller(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t leds)
 {
   static uint8_t last_seg1 = 0, last_seg2 = 0, last_seg3 = 0, last_leds = 0;
@@ -94,7 +110,7 @@ void SecretLabMagnusPro::process_controller(uint8_t seg1, uint8_t seg2, uint8_t 
   last_seg3 = seg3;
   last_leds = leds;
 
-  ESP_LOGD(TAG, "controller: %02x %02x %02x %02x", seg1, seg2, seg3, leds);
+  ESP_LOGD(TAG, "controller: %c %c %c %02x", _7seg_to_char(seg1), _7seg_to_char(seg2), _7seg_to_char(seg3), leds);
 }
 
 void SecretLabMagnusPro::process_remote(uint8_t unk, uint8_t keys)
