c43de04e50e3915229a2ec1e9856364237e5c46b
Your Name <you@example.com>
2025-08-09 02:12:17 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -13,6 +13,17 @@ void SecretLabMagnusPro::setup()
 
 void SecretLabMagnusPro::loop()
 {
+  while (this->controller_->available())
+  {
+    uint8_t byte = this->controller_->read();
+    ESP_LOGD(TAG, "controller: %02x", byte);
+  }
+
+  while (this->remote_->available())
+  {
+    uint8_t byte = this->remote_->read();
+    ESP_LOGD(TAG, "remote: %02x", byte);
+  }
 }
 
 void SecretLabMagnusPro::dump_config()
