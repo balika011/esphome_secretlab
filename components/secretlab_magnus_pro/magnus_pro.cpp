a79e7bd8775b28afdead0fa7e836112629865cf5
Your Name <you@example.com>
2025-08-09 12:12:25 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -16,7 +16,7 @@ void SecretLabMagnusPro::loop()
 
   recv_controller();
 
-  // recv_remote();
+  recv_remote();
 }
 
 void SecretLabMagnusPro::dump_config()
@@ -47,20 +47,40 @@ void SecretLabMagnusPro::recv_controller()
 
   if (checksum != msg[4])
   {
-    printf("controller: Invalid checksum! %02x != %02x", checksum, msg[4]);
+    ESP_LOGD(TAG, "controller: Invalid checksum! %02x != %02x", checksum, msg[4]);
   }
 
-  printf("controller: %02x %02x %02x %02x", msg[0], msg[1], msg[2], msg[3]);
+  ESP_LOGD(TAG, "controller: %02x %02x %02x %02x", msg[0], msg[1], msg[2], msg[3]);
 }
 
 void SecretLabMagnusPro::recv_remote()
 {
-  if (this->remote_->available())
+  if (this->remote_->available() < 5)
   {
-    uint8_t byte;
-    this->remote_->read_byte(&byte);
-    ESP_LOGD(TAG, "remote: %02x", byte);
+    ESP_LOGD(TAG, "remote: available: %02x", this->remote_->available());
+    return;
   }
+
+  uint8_t byte;
+  this->remote_->read_byte(&byte);
+  if (byte != 0xa5)
+  {
+    ESP_LOGD(TAG, "remote: %02x != a5", byte);
+    return;
+  }
+
+  uint8_t msg[4];
+  this->remote_->read_array(msg, sizeof(msg));
+  uint8_t checksum = 0;
+  for (int i = 0; i < sizeof(msg) - 1; i++)
+    checksum += msg[i];
+
+  if (checksum != msg[3])
+  {
+    ESP_LOGD(TAG, "remote: Invalid checksum! %02x != %02x", checksum, msg[4]);
+  }
+
+  ESP_LOGD(TAG, "remote: %02x %02x %02x %02x", msg[0], msg[1], msg[2], msg[3]);
 }
 
 } //namespace secretlab
