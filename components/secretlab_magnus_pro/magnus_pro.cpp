312eacc925eb9dcdccdbb5da6246c866c44beac7
Your Name <you@example.com>
2025-08-09 11:58:31 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -12,73 +12,23 @@ void SecretLabMagnusPro::setup()
 
 void SecretLabMagnusPro::loop()
 {
-  recv_controller();
+  while (this->controller_->available())
+  {
+    uint8_t byte;
+    this->controller_->read_byte(&byte);
+    ESP_LOGD(TAG, "controller: %02x", byte);
+  }
 
-  recv_remote();
+  while (this->remote_->available())
+  {
+    uint8_t byte;
+    this->remote_->read_byte(&byte);
+    ESP_LOGD(TAG, "remote: %02x", byte);
+  }
 }
 
 void SecretLabMagnusPro::dump_config()
 {
-}
-
-void SecretLabMagnusPro::recv_controller()
-{
-  if (this->controller_->available() < 6)
-  {
-    ESP_LOGD(TAG, "controller: available: %02x", this->controller_->available());
-    return;
-  }
-
-  uint8_t byte;
-  this->controller_->read_byte(&byte);
-  if (byte != 0x5a)
-  {
-    ESP_LOGD(TAG, "controller: %02x != 5a", byte);
-    return;
-  }
-
-  uint8_t msg[5];
-  this->controller_->read_array(msg, sizeof(msg));
-  uint8_t checksum = 0;
-  for (int i = 0; i < sizeof(msg) - 1; i++)
-    checksum += msg[i];
-
-  if (checksum != msg[4])
-  {
-    printf("controller: Invalid checksum! %02x != %02x", checksum, msg[4]);
-  }
-
-  printf("controller: %02x %02x %02x %02x", msg[0], msg[1], msg[2], msg[3]);
-}
-
-void SecretLabMagnusPro::recv_remote()
-{
-  if (this->remote_->available() < 5)
-  {
-    ESP_LOGD(TAG, "remote: available: %02x", this->remote_->available());
-    return;
-  }
-
-  uint8_t byte;
-  this->remote_->read_byte(&byte);
-  if (byte != 0xa5)
-  {
-    ESP_LOGD(TAG, "remote: %02x != a5", byte);
-    return;
-  }
-
-  uint8_t msg[4];
-  this->remote_->read_array(msg, sizeof(msg));
-  uint8_t checksum = 0;
-  for (int i = 0; i < sizeof(msg) - 1; i++)
-    checksum += msg[i];
-
-  if (checksum != msg[3])
-  {
-    printf("remote: Invalid checksum! %02x != %02x", checksum, msg[4]);
-  }
-
-  printf("remote: %02x %02x %02x %02x", msg[0], msg[1], msg[2], msg[3]);
 }
 
 } //namespace secretlab
