db70504be93084849472ca7cf01fd7406afaf4b9
Your Name <you@example.com>
2025-08-10 22:55:55 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -8,6 +8,7 @@ static const char *const TAG = "secretlab.magnus_pro";
 
 void IRAM_ATTR gpio_intr(SecretLabMagnusPro *arg) {
   bool new_state = arg->isr_pin_.digital_read();
+  arg->controller_key_->digital_write(new_state);
 }
 
 void SecretLabMagnusPro::setup()
--- components/secretlab_magnus_pro/magnus_pro.h
+++ components/secretlab_magnus_pro/magnus_pro.h
@@ -21,7 +21,7 @@ class SecretLabMagnusPro : public Component
   void set_controller(uart::UARTComponent *controller) { this->controller_ = controller; }
   void set_controller_key(GPIOPin *pin) { controller_key_ = pin; }
   void set_remote(uart::UARTComponent *remote) { this->remote_ = remote; }
-  void set_remote_key(GPIOPin *pin) { remote_key_ = auto *internal_pin = static_cast<InternalGPIOPin *>(pin); }
+  void set_remote_key(GPIOPin *pin) { remote_key_ = static_cast<InternalGPIOPin *>(pin); }
 
  //protected:
   void recv_controller();
