2ff987217b44d3baf551b6d8064bbe9d486f42ec
Your Name <you@example.com>
2025-08-11 02:03:52 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -45,6 +45,8 @@ void SecretLabMagnusPro::loop()
 
   static uint8_t seg1 = 0, seg2 = 0, seg3 = 0, leds = 0;
 
+  seg1 = seg2 = seg3 = 0xff;
+
   uint8_t fake_display[] = { 0x5a, seg1, seg2, seg3, leds, (seg1 + seg2 + seg3 + leds) };
   this->remote_->write_array(fake_display, sizeof(fake_display));
 
