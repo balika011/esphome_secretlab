174d072d6bb69d7b37a7e1b708120ecd08b7c093
Balázs Triszka <info@balika011.hu>
2025-10-24 03:30:56 +0200

    .

--- components/secretlab_magnus_pro/magnus_pro.cpp
+++ components/secretlab_magnus_pro/magnus_pro.cpp
@@ -5,227 +5,224 @@ namespace esphome
 {
 namespace secretlab
 {
+static const char *const TAG = "secretlab.magnus_pro";
 
-	static const char *const TAG = "secretlab.magnus_pro";
+enum REMOTE_KEYS
+{
+	KEY_S = (1 << 0),
+	KEY_1 = (1 << 1),
+	KEY_2 = (1 << 2),
+	KEY_3 = (1 << 3),
+	KEY_UP = (1 << 5),
+	KEY_DOWN = (1 << 6),
+};
 
-	void IRAM_ATTR _gpio_intr(SecretLabMagnusPro *arg)
-	{
-		arg->gpio_intr();
-	}
+enum REMOTE_LEDS
+{
+	LED_UP = (1 << 0),
+	LED_DOWN = (1 << 1),
+	LED_S = (1 << 3),
+	LED_1 = (1 << 4),
+	LED_2 = (1 << 5),
+	LED_3 = (1 << 6)
+};
 
-	void SecretLabMagnusPro::setup()
-	{
-		this->controller_key_->setup();
-		this->remote_key_->setup();
+static char _7seg_to_char(uint8_t seg)
+{
+	static const uint8_t alpha_7seg[] = {0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x3d, 0x74, 0x30, 0x1e, 0x75, 0x38, 0x15, 0x37, 0x3f, 0x73, 0x67, 0x33, 0x6d, 0x78, 0x3e, 0x2e, 0x2a, 0x76, 0x6e, 0x4b};
+	static const uint8_t num_7seg[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
 
-		this->controller_key_->digital_write(this->remote_key_->digital_read());
+	if (seg == 0x00)
+		return ' ';
 
-		this->isr_pin_ = this->remote_key_->to_isr();
+	if (seg == 0x40)
+		return '-';
 
-		this->remote_key_->attach_interrupt(&_gpio_intr, this, gpio::INTERRUPT_ANY_EDGE);
-	}
+	for (int i = 0; i < sizeof(alpha_7seg); i++)
+		if (alpha_7seg[i] == (seg & 0x7f))
+			return 'A' + i;
 
-	void SecretLabMagnusPro::loop()
-	{
+	for (int i = 0; i < sizeof(num_7seg); i++)
+		if (num_7seg[i] == (seg & 0x7f))
+			return '0' + i;
+
+	ESP_LOGE(TAG, "unknown character: %02x", seg);
+
+	return '?';
+}
+
+void IRAM_ATTR _gpio_intr(SecretLabMagnusPro *arg)
+{
+	arg->gpio_intr();
+}
+
+void SecretLabMagnusPro::setup()
+{
+	this->controller_key_->setup();
+	this->remote_key_->setup();
+
+	this->controller_key_->digital_write(this->remote_key_->digital_read());
+
+	this->isr_pin_ = this->remote_key_->to_isr();
+
+	this->remote_key_->attach_interrupt(&_gpio_intr, this, gpio::INTERRUPT_ANY_EDGE);
+}
+
+void SecretLabMagnusPro::loop()
+{
+	recv_controller();
+	recv_remote();
+
+	send_controller();
+	send_remote();
+
 #if 0
-		bool new_state = this->remote_key_->digital_read();
-		if (new_state != last_state_)
-		{
-			last_state_ = new_state;
-			ESP_LOGD(TAG, "loop: %d", new_state);
-			this->controller_key_->digital_write(new_state);
-		}
+	static uint8_t seg1 = 0, seg2 = 0, seg3 = 0, leds = 0;
+
+	uint8_t fake_display[] = { 0x5a, seg1, seg2, seg3, leds, (seg1 + seg2 + seg3 + leds) };
+	this->remote_->write_array(fake_display, sizeof(fake_display));
+
+	seg1 <<= 1;
+
+	if (seg1 == 0)
+		seg2 = 1;
+
+	seg2 <<= 1;
+
+	if (seg2 == 0)
+		seg3 = 1;
+
+	seg3 <<= 1;
+
+	if (seg3 == 0)
+		seg1 = 1;
+
+	leds <<= 1;
+
+	if (leds == 0)
+		leds = 1;
 #endif
+}
 
-		recv_controller();
+void SecretLabMagnusPro::dump_config()
+{
+}
 
-		recv_remote();
+void SecretLabMagnusPro::recv_controller()
+{
+	if (this->controller_->available() < 6)
+		return;
 
-#if 0
-		static uint8_t seg1 = 0, seg2 = 0, seg3 = 0, leds = 0;
-
-		uint8_t fake_display[] = { 0x5a, seg1, seg2, seg3, leds, (seg1 + seg2 + seg3 + leds) };
-		this->remote_->write_array(fake_display, sizeof(fake_display));
-
-		seg1 <<= 1;
-
-		if (seg1 == 0)
-			seg2 = 1;
-
-		seg2 <<= 1;
-
-		if (seg2 == 0)
-			seg3 = 1;
-
-		seg3 <<= 1;
-
-		if (seg3 == 0)
-			seg1 = 1;
-
-		leds <<= 1;
-
-		if (leds == 0)
-			leds = 1;
-#endif
+	uint8_t byte;
+	this->controller_->read_byte(&byte);
+	if (byte != 0x5a)
+	{
+		ESP_LOGD(TAG, "controller: %02x != 5a", byte);
+		return;
 	}
 
-	void SecretLabMagnusPro::dump_config()
+	uint8_t msg[5];
+	this->controller_->read_array(msg, sizeof(msg));
+	uint8_t checksum = 0;
+	for (int i = 0; i < sizeof(msg) - 1; i++)
+		checksum += msg[i];
+
+	if (checksum != msg[4])
 	{
+		ESP_LOGD(TAG, "controller: Invalid checksum! %02x != %02x", checksum, msg[4]);
+		return;
 	}
 
-	void SecretLabMagnusPro::recv_controller()
+	process_controller(msg[0], msg[1], msg[2], msg[3]);
+}
+
+void SecretLabMagnusPro::send_controller()
+{
+	uint8_t data[] = {0xa5, last_unk_, last_keys_, ~last_keys_, last_unk_ + last_keys_ + ~last_keys_};
+	this->controller_->write_array(data, sizeof(data));
+}
+
+void SecretLabMagnusPro::recv_remote()
+{
+	if (this->remote_->available() < 5)
+		return;
+
+	uint8_t byte;
+	this->remote_->read_byte(&byte);
+	if (byte != 0xa5)
 	{
-		if (this->controller_->available() < 6)
-			return;
-
-		uint8_t byte;
-		this->controller_->read_byte(&byte);
-		if (byte != 0x5a)
-		{
-			ESP_LOGD(TAG, "controller: %02x != 5a", byte);
-			return;
-		}
-
-		uint8_t msg[5];
-		this->controller_->read_array(msg, sizeof(msg));
-		uint8_t checksum = 0;
-		for (int i = 0; i < sizeof(msg) - 1; i++)
-			checksum += msg[i];
-
-		if (checksum != msg[4])
-		{
-			ESP_LOGD(TAG, "controller: Invalid checksum! %02x != %02x", checksum, msg[4]);
-			return;
-		}
-
-		process_controller(msg[0], msg[1], msg[2], msg[3]);
+		ESP_LOGD(TAG, "remote: %02x != a5", byte);
+		return;
 	}
 
-	void SecretLabMagnusPro::recv_remote()
+	uint8_t msg[4];
+	this->remote_->read_array(msg, sizeof(msg));
+	uint8_t checksum = 0;
+	for (int i = 0; i < sizeof(msg) - 1; i++)
+		checksum += msg[i];
+
+	if (checksum != msg[3])
 	{
-		if (this->remote_->available() < 5)
-			return;
-
-		uint8_t byte;
-		this->remote_->read_byte(&byte);
-		if (byte != 0xa5)
-		{
-			ESP_LOGD(TAG, "remote: %02x != a5", byte);
-			return;
-		}
-
-		uint8_t msg[4];
-		this->remote_->read_array(msg, sizeof(msg));
-		uint8_t checksum = 0;
-		for (int i = 0; i < sizeof(msg) - 1; i++)
-			checksum += msg[i];
-
-		if (checksum != msg[3])
-		{
-			ESP_LOGD(TAG, "remote: Invalid checksum! %02x != %02x", checksum, msg[3]);
-			return;
-		}
-
-		if (msg[1] != (uint8_t)~msg[2])
-		{
-			ESP_LOGD(TAG, "controller: Keymap not match negated! %02x != %02x", msg[1], (uint8_t)~msg[2]);
-			return;
-		}
-
-		process_remote(msg[0], msg[1]);
+		ESP_LOGD(TAG, "remote: Invalid checksum! %02x != %02x", checksum, msg[3]);
+		return;
 	}
 
-	uint8_t alpha_7seg[] = {0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x3d, 0x74, 0x30, 0x1e, 0x75, 0x38, 0x15, 0x37, 0x3f, 0x73, 0x67, 0x33, 0x6d, 0x78, 0x3e, 0x2e, 0x2a, 0x76, 0x6e, 0x4b};
-	uint8_t num_7seg[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
-
-	static char _7seg_to_char(uint8_t seg)
+	if (msg[1] != (uint8_t)~msg[2])
 	{
-		if (seg == 0x00)
-			return ' ';
-
-		if (seg == 0x40)
-			return '-';
-
-		for (int i = 0; i < sizeof(alpha_7seg); i++)
-			if (alpha_7seg[i] == (seg & 0x7f))
-				return 'A' + i;
-
-		for (int i = 0; i < sizeof(num_7seg); i++)
-			if (num_7seg[i] == (seg & 0x7f))
-				return '0' + i;
-
-		ESP_LOGE(TAG, "unknown character: %02x", seg);
-
-		return '?';
+		ESP_LOGD(TAG, "controller: Keymap not match negated! %02x != %02x", msg[1], (uint8_t)~msg[2]);
+		return;
 	}
 
-	enum REMOTE_LEDS
-	{
-		LED_UP = (1 << 0),
-		LED_DOWN = (1 << 1),
-		LED_S = (1 << 3),
-		LED_1 = (1 << 4),
-		LED_2 = (1 << 5),
-		LED_3 = (1 << 6)
-	};
+	process_remote(msg[0], msg[1]);
+}
 
-	void SecretLabMagnusPro::process_controller(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t leds)
-	{
-		if (this->last_seg1_ == seg1 && this->last_seg2_ == seg2 && this->last_seg3_ == seg3 && this->last_leds_ == leds)
-			return;
+void SecretLabMagnusPro::send_remote()
+{
+	uint8_t data[] = {0x5a, last_seg1_, last_seg2_, last_seg3_, last_leds_, (last_seg1_ + last_seg2_ + last_seg3_ + last_leds_)};
+	this->remote_->write_array(data, sizeof(data));
+}
 
-		this->last_seg1_ = seg1;
-		this->last_seg2_ = seg2;
-		this->last_seg3_ = seg3;
-		this->last_leds_ = leds;
+void SecretLabMagnusPro::process_controller(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t leds)
+{
+	if (this->last_seg1_ == seg1 && this->last_seg2_ == seg2 && this->last_seg3_ == seg3 && this->last_leds_ == leds)
+		return;
 
-		std::string disp;
-		disp += _7seg_to_char(seg1);
-		if (seg1 & 0x80)
-			disp += '.';
-		disp += _7seg_to_char(seg2);
-		if (seg2 & 0x80)
-			disp += '.';
-		disp += _7seg_to_char(seg3);
-		if (seg3 & 0x80)
-			disp += '.';
+	this->last_seg1_ = seg1;
+	this->last_seg2_ = seg2;
+	this->last_seg3_ = seg3;
+	this->last_leds_ = leds;
 
-		ESP_LOGD(TAG, "controller: %s %02x", disp.c_str(), leds);
+	std::string disp;
+	disp += _7seg_to_char(seg1);
+	if (seg1 & 0x80)
+		disp += '.';
+	disp += _7seg_to_char(seg2);
+	if (seg2 & 0x80)
+		disp += '.';
+	disp += _7seg_to_char(seg3);
+	if (seg3 & 0x80)
+		disp += '.';
 
-		uint8_t fake_display[] = {0x5a, seg1, seg2, seg3, leds, (seg1 + seg2 + seg3 + leds)};
-		this->remote_->write_array(fake_display, sizeof(fake_display));
-	}
+	ESP_LOGD(TAG, "controller: %s %02x", disp.c_str(), leds);
+}
 
-	enum REMOTE_KEYS
-	{
-		KEY_S = (1 << 0),
-		KEY_1 = (1 << 1),
-		KEY_2 = (1 << 2),
-		KEY_3 = (1 << 3),
-		KEY_UP = (1 << 5),
-		KEY_DOWN = (1 << 6),
-	};
+void SecretLabMagnusPro::process_remote(uint8_t unk, uint8_t keys)
+{
+	if (this->last_unk_ == unk && this->last_keys_ == keys)
+		return;
 
-	void SecretLabMagnusPro::process_remote(uint8_t unk, uint8_t keys)
-	{
-		uint8_t remote_standby[] = {0xa5, unk, keys, ~keys, unk + keys + ~keys};
-		this->controller_->write_array(remote_standby, sizeof(remote_standby));
+	this->last_unk_ = unk;
+	this->last_keys_ = keys;
 
-		if (this->last_unk_ == unk && this->last_keys_ == keys)
-			return;
+	ESP_LOGD(TAG, "remote: %02x %02x", unk, keys);
+}
 
-		this->last_unk_ = unk;
-		this->last_keys_ = keys;
-
-		ESP_LOGD(TAG, "remote: %02x %02x", unk, keys);
-	}
-
-	void SecretLabMagnusPro::gpio_intr()
-	{
-		bool new_state = this->isr_pin_.digital_read();
-		ESP_LOGD(TAG, "gpio_intr: %d", new_state);
-		this->controller_key_->digital_write(new_state);
-	}
+void SecretLabMagnusPro::gpio_intr()
+{
+	bool new_state = this->isr_pin_.digital_read();
+	ESP_LOGD(TAG, "gpio_intr: %d", new_state);
+	this->controller_key_->digital_write(new_state);
+}
 
 } // namespace secretlab
 } // namespace esphome
--- components/secretlab_magnus_pro/magnus_pro.h
+++ components/secretlab_magnus_pro/magnus_pro.h
@@ -8,40 +8,42 @@
 #include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
 
-namespace esphome {
-namespace secretlab {
-
+namespace esphome
+{
+namespace secretlab
+{
 class SecretLabMagnusPro : public Component
 {
 public:
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
+	void setup() override;
+	void loop() override;
+	void dump_config() override;
 
-  void set_controller(uart::UARTComponent *controller) { this->controller_ = controller; }
-  void set_controller_key(GPIOPin *pin) { controller_key_ = pin; }
-  void set_remote(uart::UARTComponent *remote) { this->remote_ = remote; }
-  void set_remote_key(InternalGPIOPin *pin) { remote_key_ = pin; }
-  void gpio_intr();
+	void set_controller(uart::UARTComponent *controller) { this->controller_ = controller; }
+	void set_controller_key(GPIOPin *pin) { controller_key_ = pin; }
+	void set_remote(uart::UARTComponent *remote) { this->remote_ = remote; }
+	void set_remote_key(InternalGPIOPin *pin) { remote_key_ = pin; }
+	void gpio_intr();
 
 protected:
-  void recv_controller();
-  void recv_remote();
+	void recv_controller();
+	void send_controller();
+	void recv_remote();
+	void send_remote();
 
-  void process_controller(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t leds);
-  void process_remote(uint8_t unk, uint8_t keys);
+	void process_controller(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t leds);
+	void process_remote(uint8_t unk, uint8_t keys);
 
-  uart::UARTComponent *controller_ = 0;
-  GPIOPin *controller_key_ = 0;
+	uart::UARTComponent *controller_ = 0;
+	GPIOPin *controller_key_ = 0;
 
-  uart::UARTComponent *remote_ = 0;
-  InternalGPIOPin *remote_key_ = 0;
-  ISRInternalGPIOPin isr_pin_;
-  bool last_state_ = false;
+	uart::UARTComponent *remote_ = 0;
+	InternalGPIOPin *remote_key_ = 0;
+	ISRInternalGPIOPin isr_pin_;
+	bool last_state_ = false;
 
-  uint8_t last_seg1_ = 0, last_seg2_ = 0, last_seg3_ = 0, last_leds_ = 0;
-  uint8_t last_unk_ = 0, last_keys_ = 0;
+	uint8_t last_seg1_ = 0, last_seg2_ = 0, last_seg3_ = 0, last_leds_ = 0;
+	uint8_t last_unk_ = 0, last_keys_ = 0;
 };
-
-}  // namespace secretlab
-}  // namespace esphome
+} // namespace secretlab
+} // namespace esphome
